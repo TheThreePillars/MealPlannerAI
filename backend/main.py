1f7b803aca4a0997299c5c4843a2e9fb52af10dd
TheThreePillars <103154726+TheThreePillars@users.noreply.github.com>
2025-01-25 20:36:42 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from supabase import create_client, Client
 import os
 
+# Create FastAPI app
+app = FastAPI()
+
 supabase: Client = create_client("https://ocajouluyegepogyqcyn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYWpvdWx1eWVnZXBvZ3lxY3luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4Mzg5OTgsImV4cCI6MjA1MzQxNDk5OH0.zlRK2kGdlOSTxHf9g9u819M0kDXRr3pnGGdcg3uN0_M")
 
 @app.get("/")
